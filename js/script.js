361751a54f11f97b3c8ccfa1a3b7bda8d3712107
gawon-33 <kaon6543@gmail.com>
2026-07-08 13:50:47 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const slides = document.querySelectorAll(".slide");
+
+let current = 0;
+
+setInterval(() => {
+
+    slides[current].classList.remove("active");
+
+    current++;
+
+    if(current >= slides.length){
+        current = 0;
+    }
+
+    slides[current].classList.add("active");
+
+},4000);
